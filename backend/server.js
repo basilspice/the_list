c77dd52b9cc36dd1e3d7e109cd09a1fa04dfbb37
basil <bizzlebreton@gmail.com>
2022-08-18 12:44:04 -0700

    started project

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const notes = require("./data/notes");
+const dotenv = require("dotenv");
+// const connectDB = require("./config/db")
+const userRoutes = require("./routes/userRoutes");
+
+
+
+const app = express();
+dotenv.config();
+// connectDB();
+app.use(express.json())
+
+app.get("/", (req, res) => {
+  res.send("API is running");
+});
+
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
+
+app.use("/api/users", userRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
