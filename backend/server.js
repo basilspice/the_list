9c07f669fbe5ceed0863ff2eca0651a15713a209
basil <bizzlebreton@gmail.com>
2022-08-22 13:21:30 -0700

    connected mongo db, front end is able to make axios calls, working accordion

--- backend/server.js
+++ backend/server.js
@@ -1,26 +1,30 @@
 const express = require("express");
-const notes = require("./data/notes");
+const lists = require("./data/lists");
 const dotenv = require("dotenv");
-// const connectDB = require("./config/db")
+const connectDB = require("./config/db")
 const userRoutes = require("./routes/userRoutes");
 
 
 
 const app = express();
 dotenv.config();
-// connectDB();
+connectDB();
 app.use(express.json())
 
 app.get("/", (req, res) => {
   res.send("API is running");
 });
 
-app.get("/api/notes", (req, res) => {
-  res.json(notes);
+app.get("/api/lists", (req, res) => {
+  res.json(lists);
+});
+
+app.get("/api/lists/:id", (req, res) => {
+  res.json(lists);
 });
 
 app.use("/api/users", userRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
